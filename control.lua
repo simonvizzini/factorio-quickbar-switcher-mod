3d79a35ae12d3f6944159aa754e04f5409338185
Simon Vizzini <simon.vizzini83@gmail.com>
2017-06-11 17:21:23 +0200

    improve layout switching logic so that only 1 empty slot is required for a full layout switch

--- control.lua
+++ control.lua
@@ -46,41 +46,64 @@ local function make_hotkey_handler(target_index)
             return
         end
 
-        utils.log("global.player_data dump: " .. serpent.dump(global.player_data))
-
-        -- TODO:
-        -- Implement smart inventory moving that considers currently free quickbar slots,
-        -- required slots for the target layout and items with only 1 stack available that are shared between quickbars.
         local quickbar = player.character.get_inventory(defines.inventory.player_quickbar)
         local quickbar_slot_count = #quickbar
-        local quickbar_item_count = utils.count_inventory_items(quickbar)
-        -- local quickbar_free_slots = quickbar_slot_count - quickbar_item_count
-
-        -- pprint("quickbar slot count: " .. quickbar_slot_count)
-        -- pprint("quickbar used slots: " .. quickbar_item_count)
-        -- pprint("free quickbar slots: " .. quickbar_free_slots)
+        local quickbar_items = utils.filter(quickbar, function (slot) return slot.valid and slot.valid_for_read end)
+        local quickbar_item_count = #quickbar_items
 
         local inventory = player.character.get_inventory(defines.inventory.player_main)
         local inventory_slot_count = #inventory
         local inventory_item_count = utils.count_inventory_items(inventory)
         local inventory_free_slots = inventory_slot_count - inventory_item_count
 
-        -- pprint("inventory slot count: " .. inventory_slot_count)
-        -- pprint("inventory used slots: " .. inventory_item_count)
-        -- pprint("free inventory slots: " .. inventory_free_slots)
-
         local target_layout = player_data.layouts[target_index] or {}
-        -- local target_layout_item_count = #target_layout
+        local target_layout_items = utils.filter(target_layout, function (item) return item.name ~= nil end)
+        local target_layout_item_count = #target_layout_items
 
-        if inventory_free_slots < quickbar_item_count then
-            local missing_slots = quickbar_item_count - inventory_free_slots
+        utils.log("target layout item count: " .. target_layout_item_count)
+
+        -- how many items can we carry over from the current layout to the target layout?
+        -- required to know how many free inventory slots are necessary to switch layouts
+        local items_to_carry_over = utils.intersection(
+            quickbar_items,
+            target_layout_items,
+            function (quickbar_slot, target_item)
+                return quickbar_slot.name == target_item.name
+            end
+        )
+
+        utils.log("items to carry over: " .. #items_to_carry_over)
+
+        local required_inventory_slots = quickbar_item_count - target_layout_item_count - #items_to_carry_over
+
+        utils.log("required inventory slots: " .. required_inventory_slots)
+
+        if inventory_free_slots < required_inventory_slots then
+            local missing_slots = required_inventory_slots - inventory_free_slots
             pprint("Cannot switch, not enough empty inventory slots. Need " .. missing_slots .. " more slot" .. (missing_slots > 1 and "s" or ""))
             return
         end
 
-        --------------------------------------------------
+        local cursor_stack = utils.get_at(player, {"character", "cursor_stack"})
+        local cursor_not_empty = cursor_stack and cursor_stack.valid_for_read
+
+        ----------------------------------------------------------------------------------------------------
+        -- if there is no empty slot to temporary hold items then print a message and return.
+        -- with the check above this should never be true, but just in case I messed up
+        ----------------------------------------------------------------------------------------------------
+        if inventory_free_slots == 0 and cursor_not_empty then
+            pprint("Cannot switch, need 1 empty slot to temporary hold items.")
+            return
+        end
+
+        -- this will prevent the cursor stack from being used if the player currently holds something
+        if cursor_not_empty then
+            cursor_stack = nil
+        end
+
+        ----------------------------------------------------------------------------------------------------
         -- store the current active quickbar layout
-        --------------------------------------------------
+        ----------------------------------------------------------------------------------------------------
         local current_layout = {}
 
         for i = 1, quickbar_slot_count do
@@ -95,72 +118,92 @@ local function make_hotkey_handler(target_index)
                     filter = filter
                 }
             )
+
+            -- clear filter
+            if filter then
+                quickbar.set_filter(i, nil)
+            end
         end
 
-        utils.dump("current layout\n:", current_layout)
-        utils.dump("target layout:\n", target_layout)
+        utils.dump("\ncurrent layout", current_layout)
+        utils.dump("target layout", target_layout)
 
         --------------------------------------------------
         -- move quickbar items to inventory
         --------------------------------------------------
         for i = 1, quickbar_slot_count do
             local quickbar_slot = quickbar[i]
+            local target_item = target_layout[i] or {}
 
-            -- clear filter
-            if quickbar.get_filter(i) then
-                quickbar.set_filter(i, nil)
-            end
+            -- utils.log("slot " .. i .. "\nquickbar item: " .. (quickbar_slot.valid_for_read and quickbar_slot.name or "nil") .. "\ntarget item: " .. (target_item.name or "nil"))
 
-            if quickbar_slot.valid_for_read then
-                local empty_slot = utils.find_empty_slot(inventory)
+            if quickbar_slot.valid_for_read and target_item.name ~= quickbar_slot.name then
+                local empty_slot = cursor_stack or utils.find_empty_slot(inventory) or utils.find_empty_slot(quickbar, i)
+
+                -- check if we can temporary use a quickslot ahead to store the current item
+                if not empty_slot then
+                    empty_slot = utils.find_empty_slot(quickbar, i)
+                end
 
                 if not empty_slot then
-                    -- should not happen because of the safe guard above
-                    pprint("no empty inventory slot found")
+                    pprint("could not find slot to temporary hold item, please report if you read this.")
                     return
                 end
 
                 if not empty_slot.can_set_stack(quickbar_slot) then
-                    pprint("cannot set inventory stack, now what?")
+                    pprint("no empty slot or cannot set inventory stack, please report if you read this..")
                     return
                 end
 
                 empty_slot.set_stack(quickbar_slot)
                 quickbar_slot.clear()
             end
-        end
 
-        --------------------------------------------------
-        -- put items from target layout into quickbar
-        --------------------------------------------------
-        for i = 1, quickbar_slot_count do
-            local quickbar_slot = quickbar[i]
-            local item = target_layout[i]
+            if not quickbar_slot.valid_for_read and target_item.name then
+                -- first look in inventory, then in the quickbar slots ahead
+                local item_stack = utils.find_item_stack(quickbar, target_item.name, i) or inventory.find_item_stack(target_item.name)
 
-            if item then
-                if item.name then
-                    local item_stack = inventory.find_item_stack(item.name)
-
-                    if not item_stack then
-                        pprint("couldn't find item " .. item.name .. " in inventory, skipping")
-                    elseif not quickbar_slot.can_set_stack(item_stack) then
-                        pprint("can't set quickbar stack, now what? item: " .. item.name)
-                    else
-                        quickbar_slot.set_stack(item_stack)
-                        item_stack.clear()
-                    end
+                if not item_stack then
+                    utils.log("couldn't find item " .. target_item.name .. " in inventory, skipping")
+                elseif not quickbar_slot.can_set_stack(item_stack) then
+                    -- this should also not happen anymore. the only time this did not work was
+                    -- when the target quickbar slot was filtered, but all quickbar filters ahead are reset at this point.
+                    -- but who knows, maybe there are other scenarios I don't know about yet
+                    pprint("can't set quickbar stack. item: [" .. target_item.name .. "], please report if you read this.")
+                else
+                    quickbar_slot.set_stack(item_stack)
+                    item_stack.clear()
                 end
+            end
 
-                -- restore filter
-                if item.filter then
-                    quickbar.set_filter(i, item.filter)
+            -- reset cursor stack if we used it as temporary holder
+            if cursor_stack and cursor_stack.valid_for_read then
+                local empty_slot = utils.find_empty_slot(inventory) or utils.find_empty_slot(quickbar, i)
+                if not empty_slot then
+                    -- this should never happen, but who knows, return so no items in the cursor stack are destroyed
+                    pprint("no empty slot found to put cursor item back, please report if you read this.")
+                    return
                 end
+                if not empty_slot.can_set_stack(cursor_stack) then
+                    -- again, this should not happen anymore
+                    pprint("cannot set cursor stack, please report if you read this.")
+                    return
+                end
+                empty_slot.set_stack(cursor_stack)
+                cursor_stack.clear()
+            end
+
+            -- restore filter
+            if target_item.filter then
+                quickbar.set_filter(i, target_item.filter)
             end
         end
 
         -- store new layout and index
         player_data.layouts[current_index] = current_layout
         player_data.current_layout_index = target_index
+
+        utils.log("quickbar switch done")
     end
 end
 
--- utils.lua
+++ utils.lua
@@ -11,12 +11,22 @@ local function log(msg)
 end
 
 local function dump(msg, object)
-    log(msg .. ": " ..serpent.dump(object))
+    log(msg .. ":\n" .. serpent.dump(object) .. "\n")
 end
 
 local function is_array(collection)
     -- Maybe a bit naive to check if it's an array but it seems to work for now
     return collection[1] ~= nil
+end
+
+local function _for(from, to, collection, callback_fn)
+    for i = from, to do
+        local v = collection[i]
+        local continue = callback_fn(i, v)
+        if continue == false then
+            return
+        end
+    end
 end
 
 local function each(collection, callback_fn, skip)
@@ -27,35 +37,49 @@ local function each(collection, callback_fn, skip)
         return
     end
 
-    for i = 1 + skip, length do
-        local v = collection[i]
-        local continue = callback_fn(i, v)
-        if continue == false then
-            return
-        end
-    end
+    _for(1 + skip, length, collection, callback_fn)
 end
 
-local function find(collection, predicate_fn)
+local function each_right(collection, callback_fn, skip)
+    local length = #collection
+    skip = skip ~= nil and skip or 0
+
+    if skip >= length or length == 0 then
+        return
+    end
+
+    _for(length - skip, 1, collection, callback_fn)
+end
+
+local function _find(iterator, collection, predicate_fn, skip)
     local result = nil
-    each(
+    iterator(
         collection,
-        function (i, v)
+        function(i, v)
             local found = predicate_fn(i, v)
             if found then
                 result = found
                 return false
             end
-        end
+        end,
+        skip
     )
     return result
+end
+
+local function find(collection, predicate_fn, skip)
+    return _find(each, collection, predicate_fn, skip)
+end
+
+local function find_right(collection, predicate_fn, skip)
+    return _find(each_right, collection, predicate_fn, skip)
 end
 
 local function reduce(collection, callback_fn, initial_value)
     local acc = initial_value ~= nil and initial_value or collection[1]
     each(
         collection,
-        function (i, v)
+        function(i, v)
             acc = callback_fn(acc, v, i)
         end,
         -- skip first item if no initial value was provided
@@ -64,13 +88,38 @@ local function reduce(collection, callback_fn, initial_value)
     return acc
 end
 
-local function find_empty_slot(inventory)
+local function filter(collection, predicate_fn)
+    local result = {}
+    each(
+        collection,
+        function (i, v)
+            if predicate_fn(v, i) then
+                table.insert(result, v)
+            end
+        end
+    )
+    return result
+end
+
+local function find_empty_slot(inventory, skip)
+    return find(
+        inventory,
+        function(_, slot)
+            -- a slot is empty if valid_for_read is false
+            return slot.valid and not slot.valid_for_read and slot or nil
+        end,
+        skip
+    )
+end
+
+local function find_item_stack(inventory, name, skip)
     return find(
         inventory,
         function (_, slot)
             -- a slot is empty if valid_for_read is false
-            return slot.valid and not slot.valid_for_read and slot or nil
-        end
+            return slot.valid and slot.valid_for_read and slot.name == name and slot or nil
+        end,
+        skip
     )
 end
 
@@ -83,6 +132,63 @@ local function count_inventory_items(inventory)
         end,
         0
     )
+end
+
+local function get_at(table, path)
+    return reduce(
+        path,
+        function (curr, key)
+            if curr and curr[key] and curr[key].valid then
+                return curr[key]
+            end
+            return curr
+        end,
+        table
+    )
+end
+
+-- TODO: make this function generic again
+local function intersection(a, b, comparator_fn)
+    local result = {}
+
+    local hashmap = reduce(
+        b,
+        function (map, item)
+            if map[item.name] then
+                map[item.name] = map[item.name] + 1
+            else
+                map[item.name] = 1
+            end
+            return map
+        end,
+        {}
+    )
+
+    each(
+        a,
+        function (_, v)
+            if not hashmap[v.name] then return end
+
+            if hashmap[v.name] > 0 then
+                table.insert(result, v)
+                hashmap[v.name] = hashmap[v.name] - 1
+            end
+        end
+    )
+
+    -- while a_i <= #a and b_i <= #b do
+    --     -- TODO: use a lookup table instead
+    --     _for(b_i, #b, b, function (_, v)
+    --         if comparator_fn(a[a_i], v) then
+    --             table.insert(result, a[a_i]) -- or b?! does it matter?
+    --             b_i = b_i + 1
+    --             return false
+    --         end
+    --     end)
+    --     a_i = a_i + 1
+    -- end
+
+    return result
 end
 
 ------------------------------------------------------------------------------------
@@ -100,9 +206,15 @@ return {
     dump = dump,
     is_array = is_array,
     each = each,
+    each_right = each_right,
     find = find,
+    find_right= find_right,
     reduce = reduce,
+    filter = filter,
     find_empty_slot = find_empty_slot,
+    find_item_stack = find_item_stack,
     count_inventory_items = count_inventory_items,
+    get_at = get_at,
+    intersection = intersection,
     make_pprint = make_pprint
 }
